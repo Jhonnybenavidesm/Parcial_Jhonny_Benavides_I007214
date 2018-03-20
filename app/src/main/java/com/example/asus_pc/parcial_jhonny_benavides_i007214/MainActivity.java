53fe52323da64833079d3e38f7bb6bf8270285a9
jhonnybenavidesm <jhonny1290bena@gmail.com>
2018-03-20 13:58:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/asus_pc/parcial_jhonny_benavides_i007214/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asus_pc.parcial_jhonny_benavides_i007214;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
